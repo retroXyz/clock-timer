3dddba6f63c438fe73d37de93be2dce0f7c1ed4a
retr0_ <82467575+retroXyz@users.noreply.github.com>
2021-07-17 15:06:53 +0300

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,14 @@
+// clock.js
+// clock.realTime.
+
+// # $start?
+
+function clockTimer(){
+
+  var h = document.getElementById("hours");
+  var m = document.getElementById("minutes");
+  
+};
+
+
+// # $end?
