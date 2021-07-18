33aeec8099f9fa588836e182cd795def8cf5de36
retr0_ <82467575+retroXyz@users.noreply.github.com>
2021-07-18 14:09:13 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,10 +5,22 @@
 
 function clockTimer(){
 
-  var h = document.getElementById("hours");
-  var m = document.getElementById("minutes");
+  let h = document.getElementById("hours");
+  let m = document.getElementById("minutes");
+  let s = document.getElementById("seconds");
+  
+  // setup.getTimes.
+   // # $start?
+  
+  // setup.getTimes.
+   // # $end?
+  
+  let hours = new Date().getHours();
+  let minutes = new Date().getMinutes();
+  let seconds = new Date().getSeconds();
   
 };
+clockTimer();
 
 
 // # $end?
